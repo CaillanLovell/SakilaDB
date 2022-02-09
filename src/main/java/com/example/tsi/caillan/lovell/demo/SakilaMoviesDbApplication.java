68c3ecdea24a82ec8efe6c909811ec5dd63f1334
Caillan Lovell <clovell@softwareinstitute.com>
2022-02-09 09:39:34 +0000

    first commit

---
+++ src/main/java/com/example/tsi/caillan/lovell/demo/SakilaMoviesDbApplication.java
@@ -0,0 +1,30 @@
+package com.example.tsi.caillan.lovell.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/Home")
+public class SakilaMoviesDbApplication {
+
+	@Autowired
+	private LanguageRepository languageRepository;
+
+	public SakilaMoviesDbApplication(LanguageRepository languageRepository){
+		this.languageRepository= languageRepository;
+	}
+
+	public static void main(String[] args){
+
+		SpringApplication.run(SakilaMoviesDbApplication.class, args);
+	}
+
+	@GetMapping("/AllLanguages")
+	public @ResponseBody
+	Iterable<Language> getAllLanguages(){
+		return languageRepository.findAll();
+	}
+}
---
+++ src/test/java/com/example/tsi/caillan/lovell/demo/SakilaDatabase2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tsi.caillan.lovell.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakilaDatabase2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
