915606b4aaf397247ad3f5914b70aa255d8494da
Caillan Lovell <clovell@softwareinstitute.com>
2022-02-10 11:24:30 +0000

    new tests

--- src/test/java/com/example/tsi/caillan/lovell/demoTest/SakilaMoviesDbApplicationTests.java
+++ src/test/java/com/example/tsi/caillan/lovell/demoTest/SakilaMoviesDbApplicationTests.java
@@ -1,6 +1,7 @@
 package com.example.tsi.caillan.lovell.demoTest;
 
 
+import com.example.tsi.caillan.lovell.demo.Actor;
 import com.example.tsi.caillan.lovell.demo.City;
 import com.example.tsi.caillan.lovell.demo.Film;
 import com.example.tsi.caillan.lovell.demo.Language;
@@ -14,7 +15,8 @@ import static org.junit.Assert.assertTrue;
 class SakilaMoviesDbApplicationTests {
     private Language language = new Language();
     private Film film = new Film();
-    private City city = new City()
+    private City city = new City();
+    private Actor actor = new Actor();
 
     @Test
     public void test_getLanguage(){
@@ -34,6 +36,20 @@ class SakilaMoviesDbApplicationTests {
     public void test_getcity(){
         city.setCity("testcity");
         assertEquals("testcity",city.getCity());
+
+    }
+
+    @Test
+    public void test_getactorfirstname(){
+        actor.setFirst_name("testfirstname");
+        assertEquals("testfirstname",actor.getFirst_name());
+
+    }
+
+    @Test
+    public void test_getactorlastname(){
+        actor.setLast_name("testlastname");
+        assertEquals("testlastname",actor.getLast_name());
 
     }
 
