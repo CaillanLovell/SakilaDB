d32ceb7f44649f3970b71fe1f8f229473d16e5bd
Caillan Lovell <clovell@softwareinstitute.com>
2022-02-17 16:47:20 +0000

    change10

--- src/main/java/com/example/tsi/caillan/lovell/demo/SakilaMoviesDbApplication.java
+++ src/main/java/com/example/tsi/caillan/lovell/demo/SakilaMoviesDbApplication.java
@@ -111,7 +111,7 @@ public class SakilaMoviesDbApplication {
 //		actorRepository.delete(actor);
 //		Map<String,Boolean> response = new HashMap<>();
 //		response.put("deleted", Boolean.TRUE);
-//		return response;
+//		return respon
 
 	@GetMapping("/Cities")
 	public @ResponseBody
